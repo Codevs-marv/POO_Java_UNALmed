c455aecf33b43edc0ce0dac4b366fc92d61d02f8
MARV1904 <142747551+MARV1904@users.noreply.github.com>
2023-09-02 19:31:21 -0500

    ExerciceP23.java

--- Actividad2/Parte1/EstrDesicion_Logica/ExerciceP23.java
+++ Actividad2/Parte1/EstrDesicion_Logica/ExerciceP23.java
@@ -1,7 +1,36 @@
 package Actividad2.Parte1.EstrDesicion_Logica;
 
+import java.util.Scanner;
+
 public class ExerciceP23 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el valor de A:");
+        double A = sc.nextDouble();
+        System.out.println("Ingrese el valor de B:");
+        double B = sc.nextDouble();
+        System.out.println("Ingrese el valor de C:");
+        double C = sc.nextDouble();
         
+        // Calcula el discriminante
+        double discriminante = B * B - 4 * A * C;
+        
+        if (discriminante > 0) {
+            // Dos soluciones reales
+            double x1 = (-B + Math.sqrt(discriminante)) / (2 * A);
+            double x2 = (-B - Math.sqrt(discriminante)) / (2 * A);
+            System.out.println("Las soluciones son:");
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        } else if (discriminante == 0) {
+            // Una solución real
+            double x1 = -B / (2 * A);
+            System.out.println("La única solución es:");
+            System.out.println("x1 = " + x1);
+        } else {
+            // No hay soluciones reales
+            System.out.println("No existen soluciones reales.");
+        }
+        sc.close();
     }
 }
