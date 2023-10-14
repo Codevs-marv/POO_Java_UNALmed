9b2f9916a9946adee13dbc06aeedd2aa420d20c9
Marco Rodriguez <marcoansrv19@gmail.com>
2023-10-14 17:21:11 -0500

    Update Actividad 3

---
+++ Actividad-3/Estructura_Secuencial/ExerciceP19_Graphic/src/exercice19_graphic/Exercice19_Graphic.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercice19_graphic;
+
+/**
+ *
+ * @author marco
+ */
+public class Exercice19_Graphic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario form = new Formulario();
+        form.setVisible(true);
+        
+    }
+    
+}
