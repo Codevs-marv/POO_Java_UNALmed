46071577517a2f38263e6e6eb1aba3acdd37dc4d
MarcoRodriguez <marcoansrv19@gmail.com>
2023-10-24 20:48:31 -0500

    PromedioEstudianteInterfaz

---
+++ Actividad5/PromedioEstudianteInterfaz/Principal.java
@@ -0,0 +1,10 @@
+package Actividad5.PromedioEstudianteInterfaz;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true); 
+    }
+}
