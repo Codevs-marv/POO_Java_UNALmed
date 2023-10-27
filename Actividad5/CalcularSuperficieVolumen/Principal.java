93fa5b824a2ed54b07d8c06afbc757b4f11309ca
MarcoRodriguez <marcoansrv19@gmail.com>
2023-10-26 22:47:06 -0500

    CalcularSuperficiesVolumenes

---
+++ Actividad5/CalcularSuperficieVolumen/Principal.java
@@ -0,0 +1,13 @@
+package Actividad5.CalcularSuperficieVolumen;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true); 
+        
+        // Establece que la ventana no puede cambiar su tamaño
+        miVentanaPrincipal.setResizable(false);
+        }
+}
