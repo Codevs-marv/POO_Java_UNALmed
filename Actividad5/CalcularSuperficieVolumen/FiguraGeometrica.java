cea56a1b939575e11104e3488cbfa039d3d993fc
MarcoRodriguez <marcoansrv19@gmail.com>
2023-10-26 21:38:18 -0500

    ClasesPrincipales

---
+++ Actividad5/CalcularSuperficieVolumen/FiguraGeometrica.java
@@ -0,0 +1,23 @@
+package Actividad5.CalcularSuperficieVolumen;
+
+public class FiguraGeometrica {
+    
+    private double volumen; 
+    private double superficie; 
+
+    public void setVolumen(double volumen) {
+        this.volumen = volumen;
+    }
+
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+        
+    public double getVolumen() {
+        return this.volumen;
+    }
+       
+    public double getSuperficie() {
+        return this.superficie;
+    }
+}
